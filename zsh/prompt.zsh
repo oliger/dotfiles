856019d57f6dea1722bc274a6f1ae2427e64d2b6
Jimmy Oliger <hi@jimmyoliger.com>
2012-01-07 03:13:58 +0100

    Pimp my Prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,15 +1,34 @@
-# Adds the current branch name in green
-git_prompt_info() {
-  ref=$(git symbolic-ref HEAD 2> /dev/null)
-  if [[ -n $ref ]]; then
-    echo "[%{$fg_bold[green]%}${ref#refs/heads/}%{$reset_color%}] "
+# Allows variable substitution to take place in the prompt.
+setopt prompt_subst
+
+# Initialize colors.
+autoload -U colors && colors
+
+# If command fail, indicator become red.
+local indicator="%(?,$,%{$fg[red]%}$%{$reset_color%})"
+
+# Display the current git branch
+git_branch() {
+  echo "%{$fg[white]%}$(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})%{$reset_color%}"
+}
+
+git_status() {
+  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  if [[ $st == "" ]]
+  then
+    echo ""
+  else
+    if [[ $st == "nothing to commit (working directory clean)" ]]
+    then
+      echo "%{$fg_bold[green]%}●%{$reset_color%} "
+    else
+      echo "%{$fg_bold[red]%}●%{$reset_color%} "
+    fi
   fi
 }
 
-# Expand functions in the prompt
-setopt prompt_subst
+PROMPT='
+%~
+${indicator} %{$reset_color%}'
 
-# Prompt
-export PROMPT='
-$(git_prompt_info)%~
-▸ '
+RPROMPT='$(git_status)$(git_branch)'
