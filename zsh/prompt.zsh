e1ff08921b85a1f2c4e6021a357ec59e257e40ad
Jimmy Oliger <hi@jimmyoliger.com>
2012-01-07 04:04:06 +0100

    Make the prompt perfect

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,26 +7,32 @@ autoload -U colors && colors
 # If command fail, indicator become red.
 local indicator="%(?,$,%{$fg[red]%}$%{$reset_color%})"
 
-# Display the current git branch
-git_branch() {
-  echo "$(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})"
+# Check if is git
+is_git() {
+  if [[ $(/usr/bin/git status 2>/dev/null | tail -n 1) == "" ]]; then
+    return 1
+  else
+    return 0
+  fi
 }
 
+# Display the current git branch
+git_branch() {
+  is_git || return
+  echo " - $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})"
+}
+
+# Display a green dot if the working directory is clean and a red one if is not
+# the case.
 git_status() {
-  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
-  if [[ $st == "" ]]
-  then
-    echo ""
+  is_git || return
+  if [[ $st == "nothing to commit (working directory clean)" ]]; then
+    echo "%{$fg_bold[green]%}●%{$reset_color%} "
   else
-    if [[ $st == "nothing to commit (working directory clean)" ]]
-    then
-      echo "%{$fg_bold[green]%}●%{$reset_color%} "
-    else
-      echo "%{$fg_bold[red]%}●%{$reset_color%} "
-    fi
+    echo "%{$fg_bold[red]%}●%{$reset_color%} "
   fi
 }
 
 PROMPT='
-$(git_status) %~ on $(git_branch)
+$(git_status)%~$(git_branch)
 ${indicator} %{$reset_color%}'
