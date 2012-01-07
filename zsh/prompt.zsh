9e452946f19fa579b2bd82d41d5f91c79c4d0868
Jimmy Oliger <hi@jimmyoliger.com>
2012-01-07 04:26:32 +0100

    Remove useless function and fix git status bug in prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -7,29 +7,28 @@ autoload -U colors && colors
 # If command fail, indicator become red.
 local indicator="%(?,$,%{$fg[red]%}$%{$reset_color%})"
 
-# Check if is git
-is_git() {
-  if [[ $(/usr/bin/git status 2>/dev/null | tail -n 1) == "" ]]; then
-    return 1
-  else
-    return 0
-  fi
-}
-
 # Display the current git branch
 git_branch() {
-  is_git || return
-  echo " - $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})"
+  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  if [[ $st == "" ]]; then
+    return
+  else
+    echo " - $(/usr/bin/git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})"
+  fi
 }
 
 # Display a green dot if the working directory is clean and a red one if is not
 # the case.
 git_status() {
-  is_git || return
-  if [[ $st == "nothing to commit (working directory clean)" ]]; then
-    echo "%{$fg_bold[green]%}●%{$reset_color%} "
+  st=$(/usr/bin/git status 2>/dev/null | tail -n 1)
+  if [[ $st == "" ]]; then
+    return
   else
-    echo "%{$fg_bold[red]%}●%{$reset_color%} "
+    if [[ $st == "nothing to commit (working directory clean)" ]]; then
+      echo "%{$fg_bold[green]%}●%{$reset_color%} "
+    else
+      echo "%{$fg_bold[red]%}●%{$reset_color%} "
+    fi
   fi
 }
 
