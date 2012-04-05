d6b23376129942d29b25673e847e37c725c3bb88
Jimmy OLIGER <hi@jimmyoliger.com>
2012-04-05 23:42:13 +0200

    Update osx settings

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,58 +1,69 @@
 # Original idea
 # https://github.com/holman/dotfiles/blob/master/osx/set-defaults.sh
 # https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# https://gist.github.com/2260182
 
-# Use AirDrop over every interface. srsly this should be a default.
+echo "Use AirDrop over every interface. srsly this should be a default."
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
-# Always open everything in Finder's list view. This is important.
+echo "Always open everything in Finder's list view. This is important."
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Show the ~/Library folder
+echo "Show the ~/Library folder"
 chflags nohidden ~/Library
 
-# Automatically hide and show the Dock
+echo "Automatically hide and show the Dock"
 defaults write com.apple.dock autohide -bool true
 
-# Show remaining battery time; hide percentage
+echo "Show remaining battery time; hide percentage"
 defaults write com.apple.menuextra.battery ShowPercent -string "NO"
 defaults write com.apple.menuextra.battery ShowTime -string "YES"
 
-# Show all filename extensions in Finder
+echo "Show file extensions in Finder"
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
-# Expand save panel by default
+echo "Expand save and print panel by default"
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-
-# Expand print panel by default
 defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
 
-# Avoid creating .DS_Store files on network volumes
+echo "Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
 # Disable the warning when changing a file extension
 defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
 
-# Enable tap to click (Trackpad)
+echo "Enable tap to click"
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
 
-# Disable Safari’s thumbnail cache for History and Top Sites
+echo "Disable Safari’s thumbnail cache for History and Top Sites"
 defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
 
-# Enable Safari’s debug menu
+echo "Enable Safari’s debug menu"
 defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
 
-# Disable all the Ping stuff in iTunes
+echo "Disable all the Ping stuff in iTunes"
 defaults write com.apple.iTunes disablePingSidebar -bool true
 defaults write com.apple.iTunes disablePing -bool true
 
-# Disable Resume system-wide
+echo "Disable Resume system-wide"
 defaults write NSGlobalDomain NSQuitAlwaysKeepsWindows -bool false
 
-# Disable disk image verification
+echo "Disable disk image verification"
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-# Kill affected applications
-for app in Safari Finder Dock Mail; do killall "$app"; done
+echo "Set a blazingly fast keyboard repeat rate"
+defaults write NSGlobalDomain KeyRepeat -int 0.02
+
+echo "Set a shorter Delay until key repeat"
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
+echo "Disable auto-correct"
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+
+echo "Disable the “Are you sure you want to open this application?” dialog"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+
+echo "Kill affected applications"
+for app in Safari Finder Dock Mail SystemUIServer; do killall "$app" >/dev/null 2>&1; done
