fcbdc93b6aa19afebe996c1698c6e10bbd62ac15
Jimmy Oliger <hi@jimmyoliger.com>
2013-04-12 00:27:42 +0200

    Cleaning

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,3 @@
-alias ...="../.."
-alias mkdir="mkdir -p"
-
 alias d="cd ~/Dropbox"
 alias h="cd ~/hull"
 
@@ -9,3 +6,16 @@ alias v.="mvim ."
 
 alias o="open"
 alias o.="open ."
+
+alias mkdir="mkdir -p"
+
+# Disable correction.
+alias ack="nocorrect ack"
+alias cd="nocorrect cd"
+alias cp="nocorrect cp"
+alias grep="nocorrect grep"
+alias heroku="nocorrect heroku"
+alias ln="nocorrect ln"
+alias mkdir="nocorrect mkdir"
+alias mv="nocorrect mv"
+alias rm="nocorrect rm"
