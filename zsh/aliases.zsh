fa4e58e31f6b440405f00518e5debab74f6ef40a
Jimmy Oliger <hi@jimmyoliger.com>
2013-07-17 15:19:04 -0600

    Moar aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,22 +1,31 @@
-alias d="cd ~/Dropbox"
-alias c="cd ~/code"
-alias h="cd ~/code/hull"
+alias d='cd ~/Dropbox'
+alias c='cd ~/code'
+alias h='cd ~/code/hull'
 
-alias v="mvim"
-alias v.="mvim ."
+alias v='mvim'
+alias v.='mvim .'
 
-alias o="open"
-alias o.="open ."
+alias o='open'
+alias o.='open .'
 
-alias mkdir="mkdir -p"
+alias mkdir='mkdir -p'
+
+alias server='python -m SimpleHTTPServer'
 
 # Disable correction.
-alias ack="nocorrect ack"
-alias cd="nocorrect cd"
-alias cp="nocorrect cp"
-alias grep="nocorrect grep"
-alias heroku="nocorrect heroku"
-alias ln="nocorrect ln"
-alias mkdir="nocorrect mkdir"
-alias mv="nocorrect mv"
-alias rm="nocorrect rm"
+alias ack='nocorrect ack'
+alias cd='nocorrect cd'
+alias cp='nocorrect cp'
+alias grep='nocorrect grep'
+alias heroku='nocorrect heroku'
+alias ln='nocorrect ln'
+alias mkdir='nocorrect mkdir'
+alias mv='nocorrect mv'
+alias rm='nocorrect rm'
+
+# Git
+alias g='git'
+alias gb='git branch'
+alias gbc='git checkout -b'
+alias gbx='git branch -d'
+alias gbX='git branch -D'
