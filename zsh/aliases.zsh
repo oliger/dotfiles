7c57bf58c0e5bdb03d4e6ff4c209883ecf0060b1
Jimmy Oliger <jimmyoliger@gmail.com>
2018-10-06 13:40:03 +0200

    Clean up

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,30 +1,9 @@
-alias d='cd ~/Dropbox'
 alias s='cd ~/stuart'
 
-alias v='vim'
-alias v.='vim .'
+alias v='nvim'
+alias v.='nvim .'
 
 alias o='open'
 alias o.='open .'
 
 alias mkdir='mkdir -p'
-
-alias server='python -m SimpleHTTPServer'
-
-# Disable correction.
-alias ack='nocorrect ack'
-alias cd='nocorrect cd'
-alias cp='nocorrect cp'
-alias grep='nocorrect grep'
-alias heroku='nocorrect heroku'
-alias ln='nocorrect ln'
-alias mkdir='nocorrect mkdir'
-alias mv='nocorrect mv'
-alias rm='nocorrect rm'
-
-# Git
-alias g='git'
-alias gb='git branch'
-alias gbc='git checkout -b'
-alias gbx='git branch -d'
-alias gbX='git branch -D'
