d9fafb030b129428e89033c4c9f26dad335a78c4
Jimmy Oliger <jimmyoliger@gmail.com>
2020-05-22 23:05:18 +0200

    Set some osx settings

--- osx/install.sh
+++ osx/install.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
 sudo softwareupdate -i -a
+
+# Disable press-and-hold.
+defaults write -g ApplePressAndHoldEnabled -bool false
+
+# Fast key repeat.
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+# Open everything in list view.
+defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+
+# Use small icons in sidebar.
+defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
