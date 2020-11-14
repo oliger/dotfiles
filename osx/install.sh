7e656d47a462aed46dc727afd2dcafc4a44199f1
Jimmy Oliger <jimmyoliger@gmail.com>
2020-11-14 11:42:20 +0100

    Enable subpixel antialiasing on Big Sur

--- osx/install.sh
+++ osx/install.sh
@@ -13,3 +13,7 @@ defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
 # Use small icons in sidebar.
 defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1
+
+# Enable subpixel antialiasing
+defaults write -g AppleFontSmoothing -int 0 
+defaults write -g CGFontRenderingFontSmoothingDisabled -bool NO
