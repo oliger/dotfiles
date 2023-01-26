c239fa5099bee0c20bb7426b0c0fe3d98ba73528
Jimmy Oliger <jimmyoliger@gmail.com>
2023-01-26 22:16:24 +0100

    Fix homebrew

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -3,7 +3,7 @@
 if test ! $(which brew)
 then
   echo "Installing Homebrew"
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 exit 0
--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,2 +1,1 @@
-export PATH="/usr/local/sbin:$PATH"
-export FPATH=$(brew --prefix)/share/zsh/site-functions:$FPATH
+export PATH="/opt/homebrew/bin:$PATH"
