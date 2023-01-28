cfdc1d1650aff29715df59bdad2f894031d6cee0
Jimmy Oliger <jimmyoliger@gmail.com>
2023-01-28 18:56:12 +0100

    Stop sharing ZSH history
    Not sure why I liked this in the first place.

--- zsh/options.zsh
+++ zsh/options.zsh
@@ -1,9 +1,16 @@
 setopt AUTO_CD
+
+# Expand parameters and substitute commands in prompt.
 setopt PROMPT_SUBST
+
+# Move cursor to the end after completion.
 setopt ALWAYS_TO_END
+
+# Do not add command to the history if it is a duplicate of the previous one.
 setopt HIST_IGNORE_DUPS
+
+# Do not add command to the history if it starts with a space.
 setopt HIST_IGNORE_SPACE
-setopt SHARE_HISTORY
 
 bindkey "^A" beginning-of-line
 bindkey "^E" end-of-line
